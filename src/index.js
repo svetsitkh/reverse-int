a19033667e53415851b2c7268a8abcab89b6f0a8
svetsitkh <svetsitkh@gmail.com>
2023-09-25 21:21:02 +0300

    done task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 module.exports = function reverse (n) {
-  
+
+    const str = Math.abs(n).toString();
+    const strArray = str.split('');
+    const reversedStrArray = strArray.reverse();
+    const reversedStr = reversedStrArray.toString();
+    const newReversedStr = reversedStr.replace(/,/g, '');
+
+    return parseInt(newReversedStr);
+
 }
